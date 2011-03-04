ae293bf836e3dc282d93910b1e30fa7c0edb7e11
matt <matt@matt-ubuntu.(none)>
2011-03-04 16:50:12 -0500

    added peddle and pwm verilog files

---
+++ peddle.v
@@ -0,0 +1,16 @@
+`include "i2s.v"
+//NOTES:
+
+module peddle(data_in, data_out);
+
+//need an i2s_rx(data_in)
+
+//need clk_div module
+
+//need pwm(data_out)
+
+//need buffers
+
+
+
+endmodule
---
+++ pwm.v
@@ -0,0 +1,3 @@
+module pwm();
+
+endmodule
