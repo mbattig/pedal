33e9fe85cccac82a70a508461959089d3e07fd34
matt <matt@matt-ubuntu.(none)>
2011-03-07 10:08:00 -0500

    merging...

--- pwm.v
+++ pwm.v
@@ -1,16 +1,25 @@
-module pwm(clk, sample, out);
+module pwm(clk, sample, out, count);
 
     input [23:0] sample;
     input clk;
     output out;
+    output [4:0] count;  // 5 bits wide. take out later
 
     reg out;
-    reg [7:0] count = 8'h00;
+    reg [4:0] count = 5'b0;
+    reg [23:0] sample_holder;
     wire [23:0] sample;
     wire clk;
 
     always @ (posedge clk) begin
         count = count + 1;
+        sample_holder = sample;
+        if ((sample_holder>>19) > count) begin//*524288) begin
+            out = 1;
+        end
+        else begin
+            out = 0;
+        end
     end
 
 endmodule
--- pwm_tb.v
+++ pwm_tb.v
@@ -5,49 +5,28 @@
 
 module pwm_tb;
  
-reg mck, data_in;
-reg [23:0] test_vector = 24'h888888;
+reg mck;
+reg [23:0] sample = 24'h800000;
 
-
-wire [23:0] data_out;
+wire pwm_out;
 wire [4:0] count;
-wire data_rdy, lrck, bck, sck;
  
 initial begin
-  $dumpfile ("i2s_tb.vcd");
-  $dumpvars (1, i2s_tb);
-  $monitor ("mck=%b,lrck=%b,bck=%b,data_in=%b,data_out=%h,count=%d,data_rdy=%b,test_vector=%h", mck, lrck, bck, data_in, data_out, count, data_rdy, test_vector);
+  $dumpfile ("pwm_tb.vcd");
+  $dumpvars (1, pwm_tb);
+  $monitor ("mck=%b,sample=%h,count=%d,data=%b", mck,sample,count,pwm_out);
   mck = 0;
-  lrck = 0;
-  data_in = 0;
-  
-  //#6 data_in = 1;
-  //#120 data_in = 0;
-  #240 $finish;
+  #256 sample = 24'h000000;
+  #512 sample = 24'hffffff;
+  #768 sample = 24'h400000;
+  #1024 $finish;
 end
 
 // Generate Main Clk 
 always begin
   #1 mck = !mck;    
 end
-
-always @ (posedge lrck or negedge lrck) begin
-  test_vector = 24'h888888;
-  data_in = test_vector[23];
-end
-
-always begin
-  data_in = test_vector[23];
-  #2 test_vector = test_vector<<1;
-end
-
-
-// Generate Frame clk
-always begin
-  #60 lrck = !lrck;
-end
-
  
-i2s_recv r0(mck,lrck,bck,data_in,data_out, count, data_rdy);
+pwm pwm0(mck,sample,pwm_out,count);
  
 endmodule
